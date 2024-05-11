fbf4bba1d01efbce78d0ce143b1086008e76e8c2
Balaji-SureshM <balajisuresh009@gmail.com>
2024-05-11 12:08:44 +0530

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt  
+import pandas as pd
+import seaborn as sns
+
+from warnings import filterwarnings
+filterwarnings(action='ignore')
+
+wine = pd.read_csv("winequality-red.csv")
+print("Successfully Imported Data!")
+
+wine['goodquality'] = [1 if x >= 7 else 0 for x in wine['quality']]
+X = wine.drop(['quality','goodquality'], axis = 1)
+Y = wine['goodquality']
+
+wine['goodquality'].value_counts()
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+
+from sklearn.ensemble import ExtraTreesClassifier
+classifiern = ExtraTreesClassifier()
+classifiern.fit(X,Y)
+score = classifiern.feature_importances_
+print(score)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,random_state=7)
+
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression()
+model.fit(X_train,Y_train)
+Y_pred = model.predict(X_test)
+
+from sklearn.metrics import accuracy_score,confusion_matrix
+print("Accuracy Score:",accuracy_score(Y_test,Y_pred))
+
+confusion_mat = confusion_matrix(Y_test,Y_pred)
+print(confusion_mat)
+
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred))
+
+from sklearn.svm import SVC
+model = SVC()
+model.fit(X_train,Y_train)
+pred_y = model.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,pred_y))
+
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(criterion='entropy',random_state=7)
+model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred))
+
+from sklearn.naive_bayes import GaussianNB
+model3 = GaussianNB()
+model3.fit(X_train,Y_train)
+y_pred3 = model3.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred3))
+
+from sklearn.ensemble import RandomForestClassifier
+model2 = RandomForestClassifier(random_state=1)
+model2.fit(X_train, Y_train)
+y_pred2 = model2.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred2))
+
+import xgboost as xgb
+model5 = xgb.XGBClassifier(random_state=1)
+model5.fit(X_train, Y_train)
+y_pred5 = model5.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred5))
